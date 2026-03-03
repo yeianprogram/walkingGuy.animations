4f97ee63d75c79f7bd62cc4024075ccd49b4d481
yeianprogram <subianvu@gmail.com>
2026-03-02 19:09:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const dumpling = document.createElement("img");
 dumpling.width = 50;
 dumpling.height = 50;
-dumpling.frames {
+dumpling.frames = {
     "frame1" : "frames/1.jpeg",
     "frame2" : "frames/2.jpeg",
     "frame3" : "frames/3.jpeg"
@@ -25,7 +25,7 @@ function animate() {
   } else if (frame === 3) {
     repeat = true;
     dumpling.src = dumpling.frames.frame2;
-    let frame = 2;
+    frame = 2;
   }
 }
 setInterval(dumpling, 50);
